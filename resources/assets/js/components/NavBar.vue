7d9073764ca8b37869fb3245cafd18f7c392811a
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-09-27 05:27:32 +0200

    Laravel and Vue

---
+++ resources/assets/js/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <nav class="navbar navrbar-expand-sm navbar-dark bg-info mb-2">
+        <div class="container">
+          <a href="#" class="navbar-brand">Articles App </a>
+        </div>
+       </nav>
+    </div>
+</template>
